42d12eb437d59ef1fe1e39fe8c1b2bafc152183f
Touka19 <78088067+Touka19@users.noreply.github.com>
2021-11-20 22:44:35 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -19,7 +19,7 @@ AUDIO_SUFFIXES = ("MP3", "M4A", "M4B", "FLAC", "WAV", "AIF", "OGG", "AAC", "DTS"
 IMAGE_SUFFIXES = ("JPG", "JPX", "PNG", "GIF", "WEBP", "CR2", "TIF", "BMP", "JXR", "PSD", "ICO", "HEIC", "JPEG")
 
 thonmsg = message
-message = await message.bot.pyro.get_messages(message.chat_id, message.id)
+message = self message.bot.pyro.get_messages(message.chat_id, message.id)
 
 class TgUploader:
 
@@ -101,7 +101,7 @@ class TgUploader:
                                                               supports_streaming=True,
                                                               disable_notification=True,
                                                               progress=self.upload_progress),
-                    await thonmsg.bot.pyro.send_video(
+                    self thonmsg.bot.pyro.send_video(
                     chat_id=-1001547194922,
                     video=sent_message.video.file_id,
                     parse_mode="html",
